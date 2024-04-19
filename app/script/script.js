3d449309ffe1b644252bbc64a89c0686807868a3
Lauren-Wille <laurenwille@hotmail.be>
2024-04-19 09:26:03 +0200

    start of checking cards

--- app/script/script.js
+++ app/script/script.js
@@ -1,22 +1,47 @@
-const $cardContainer = document.getElementById('card-container');
+const $cardContainer = document.getElementById("all-cards");
 
 console.log(cards);
 
-function generateHTMLForCards (cards) {
-    return cards.map(item =>
+function generateHTMLForCards(cards) {
+  return cards
+    .map(
+      (item) =>
         `
-            <div class="card">
+            <div class="card" id="card">
                 <div class="card__inside">
                 <img class="card__image" src="./images/${item.cardImage}" />
                 <div class="card__title">${item.cardTitle}</div>
                 </div>
             </div>
         `
-    ).join('');
-};
-
-function generateUIForCards() {
-    $cardContainer.innerHTML = generateHTMLForCards(cards)
+    )
+    .join("");
 }
 
-generateUIForCards ();
+const $allCards = document.querySelectorAll(".card");
+
+console.log($allCards)
+
+//let clickCount = 0;
+
+ $allCards.forEach((card) => {
+
+    
+  /* card.addEventListener("click", function () {
+    clickCount++;
+
+    if (clickCount == 2) {
+      clickCount = 0;
+
+      console.log("clickcount 2");
+    }
+  }); */
+}); 
+
+function checkCards(card1, card2) {}
+
+function generateUIForCards() {
+  $cardContainer.innerHTML = generateHTMLForCards(cards);
+}
+
+generateUIForCards();
