083aadce9a0ad08f9d1520328177fa90fd18ec9f
tesssloc <tess.slock@student.arteveldehs.be>
2024-04-19 09:15:13 +0200

    random cards rendered

--- app/script/script.js
+++ app/script/script.js
@@ -1,6 +1,22 @@
-const $cardContainer = document.getElementById('card-container');
+const $cardContainer = document.getElementById('all-cards');
 
-console.log(cards);
+function generateRandomCards (array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+
+    let totalCards = array.slice(0, 6);
+
+    totalCards = totalCards.flatMap(i => [i, i]);
+
+    for (let i = totalCards.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [totalCards[i], totalCards[j]] = [totalCards[j], totalCards[i]];
+    }
+
+    return totalCards;
+}
 
 function generateHTMLForCards (cards) {
     return cards.map(item =>
@@ -16,7 +32,8 @@ function generateHTMLForCards (cards) {
 };
 
 function generateUIForCards() {
-    $cardContainer.innerHTML = generateHTMLForCards(cards)
+    const randomCards = generateRandomCards(cards)
+    $cardContainer.innerHTML = generateHTMLForCards(randomCards)
 }
 
-generateUIForCards ();
+generateUIForCards();
